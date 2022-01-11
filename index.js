0f5f0c816870b3a7b778d8c3971f3f369b98a77a
Ethan Isola <isolaethan@gmail.com>
2022-01-11 08:33:14 -0600

    commit tree a38260424baa1fc8c5da2d1863d0730a1a648061

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    //return drivers.slice(0,2)
+    let var1 = drivers[0];
+    let var2 = drivers[1];
+    let ar1 = [var1, var2];
+    return ar1;
+}
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice((drivers.length-2),(drivers.length))
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(inc){
+    return function fareQuintupler(fare){
+        return fare*inc;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers,pick){
+    return pick(drivers);
+}
